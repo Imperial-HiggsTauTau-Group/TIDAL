5913f37174d440912c1dc7986f91ff2b4b6f2bb5
Dougal <dougalwentworth@gmail.com>
2026-06-11 14:45:32 +0100

    Made it easier to change VSjet working point

--- Draw/scripts/4_class_BDT/HiggsTauTauPlot.py
+++ Draw/scripts/4_class_BDT/HiggsTauTauPlot.py
@@ -186,6 +186,7 @@ if args.era in early_run_3:
     tau_id_algo = 'DeepTau2018v2p5'
 else:
     tau_id_algo = 'PNet'
+wp_VSjet = '7'  # VTight
 
 if args.era in available_eras:
     if args.channel == "ee":
@@ -201,7 +202,7 @@ if args.era in available_eras:
         single_muon_only = "(trg_singlemuon && pt_1 > 26  && abs(eta_1) < 2.4)"
         trg_full = single_muon_only #"(%s || %s)" % (mt_cross_only, single_muon_only)
         categories["baseline"] = (
-            f"(m_vis>40 && iso_1 < 0.15 && id{tau_id_algo}VSjet_2 >= 7 && id{tau_id_algo}VSe_2 >= 2 && id{tau_id_algo}VSmu_2 >= 4 && %s)"
+            f"(m_vis>40 && iso_1 < 0.15 && id{tau_id_algo}VSjet_2 >= {wp_VSjet} && id{tau_id_algo}VSe_2 >= 2 && id{tau_id_algo}VSmu_2 >= 4 && %s)"
             % trg_full
         )
         if args.do_aiso:
@@ -210,8 +211,8 @@ if args.era in available_eras:
             )
             
         categories["lt_ff_AR"] = categories["baseline"].replace(
-            f"id{tau_id_algo}VSjet_2 >= 7",
-            f"id{tau_id_algo}VSjet_2 < 7 && id{tau_id_algo}VSjet_2 >= 1",
+            f"id{tau_id_algo}VSjet_2 >= {wp_VSjet}",
+            f"id{tau_id_algo}VSjet_2 < {wp_VSjet} && id{tau_id_algo}VSjet_2 >= 1",
         )
         
     if args.channel == "et":
@@ -219,7 +220,7 @@ if args.era in available_eras:
         single_electron_only = "(trg_singleelectron && pt_1 >= 32 && abs(eta_1) < 2.1 )"
         trg_full = single_electron_only # remove et cross trigger until Nanoprod v3
         categories["baseline"] = ( # Tight VSe for et
-            f"(m_vis>40 && iso_1 < 0.15 && id{tau_id_algo}VSjet_2 >= 7 && id{tau_id_algo}VSe_2 >= 6 && id{tau_id_algo}VSmu_2 >= 4 && %s)"
+            f"(m_vis>40 && iso_1 < 0.15 && id{tau_id_algo}VSjet_2 >= {wp_VSjet} && id{tau_id_algo}VSe_2 >= 6 && id{tau_id_algo}VSmu_2 >= 4 && %s)"
             % trg_full
         )
         if args.do_aiso:
@@ -228,8 +229,8 @@ if args.era in available_eras:
             )
 
         categories["lt_ff_AR"] = categories["baseline"].replace(
-            f"id{tau_id_algo}VSjet_2 >= 7",
-            f"id{tau_id_algo}VSjet_2 < 7 && id{tau_id_algo}VSjet_2 >= 1",
+            f"id{tau_id_algo}VSjet_2 >= {wp_VSjet}",
+            f"id{tau_id_algo}VSjet_2 < {wp_VSjet} && id{tau_id_algo}VSjet_2 >= 1",
         )
 
     if args.channel == "tt":
@@ -237,23 +238,23 @@ if args.era in available_eras:
         doubletaujet_only_trg = "(trg_doubletauandjet && pt_1 > 35 && pt_2 > 35 && jpt_1 > 60)"  # might need to revise jet cut later on
         trg_full = "(%s || %s)" % (doubletau_only_trg, doubletaujet_only_trg)
         categories["baseline"] = (
-            f"(m_vis > 40 && id{tau_id_algo}VSjet_1 >= 7 && id{tau_id_algo}VSjet_2 >= 7 && id{tau_id_algo}VSe_1 >= 2 && id{tau_id_algo}VSe_2 >= 2 && id{tau_id_algo}VSmu_1 >= 4 && id{tau_id_algo}VSmu_2 >= 4 && %s)"
+            f"(m_vis > 40 && id{tau_id_algo}VSjet_1 >= {wp_VSjet} && id{tau_id_algo}VSjet_2 >= {wp_VSjet} && id{tau_id_algo}VSe_1 >= 2 && id{tau_id_algo}VSe_2 >= 2 && id{tau_id_algo}VSmu_1 >= 4 && id{tau_id_algo}VSmu_2 >= 4 && %s)"
             % trg_full
         )
 
         if args.do_aiso:
             categories["baseline"] = categories["baseline"].replace(
-                f"id{tau_id_algo}VSjet_2 >= 7",
-                f"id{tau_id_algo}VSjet_2 < 7 && id{tau_id_algo}VSjet_2 >= 1",
+                f"id{tau_id_algo}VSjet_2 >= {wp_VSjet}",
+                f"id{tau_id_algo}VSjet_2 < {wp_VSjet} && id{tau_id_algo}VSjet_2 >= 1",
             )
 
         categories["tt_qcd_norm"] = categories["baseline"].replace(
-            f"id{tau_id_algo}VSjet_1 >= 7",
-            f"id{tau_id_algo}VSjet_1 < 7 && id{tau_id_algo}VSjet_1 >= 1",
+            f"id{tau_id_algo}VSjet_1 >= {wp_VSjet}",
+            f"id{tau_id_algo}VSjet_1 < {wp_VSjet} && id{tau_id_algo}VSjet_1 >= 1",
         )
         categories["tt_ff_AR"] = categories["baseline"].replace(
-            f"id{tau_id_algo}VSjet_1 >= 7",
-            f"id{tau_id_algo}VSjet_1 < 7 && id{tau_id_algo}VSjet_1 >= 1",
+            f"id{tau_id_algo}VSjet_1 >= {wp_VSjet}",
+            f"id{tau_id_algo}VSjet_1 < {wp_VSjet} && id{tau_id_algo}VSjet_1 >= 1",
         )
         categories["subleadfake"] = (
             categories["baseline"] + "&& genPartFlav_1 != 0 && genPartFlav_2 == 0"
@@ -1253,14 +1254,13 @@ elif args.channel == "et":
 else:
     qcd_factor = 1.0
 
-res_corrections = {
+res_corrections_2024 = {
     'DeepTau2018v2p5': {'5': 0.9927, '6': 0.9793, '7': 0.9647},
     'PNet': {'5': 0.9636, '6': 0.9387, '7': 0.9194},
     'UParT': {'5': 0.9508, '6': 0.9296, '7': 0.9094},
 }
 
-tau_id_wp = '7' # Using VTight
-genuine_sf = res_corrections[tau_id_algo][tau_id_wp]
+genuine_sf = res_corrections_2024[tau_id_algo][wp_VSjet] if args.era == "Run3_2024" else 1.0
 
 weight = "(weight)"
 if args.add_weight:
